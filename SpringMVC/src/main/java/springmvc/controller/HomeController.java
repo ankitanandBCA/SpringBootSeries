a46b527d374f3ccd1cda302a088222bb6db1852f
Ankit Anand <ankitanandraj9563@gmail.com>
2025-08-01 22:11:52 +0530

    Add files via upload

---
+++ SpringMVC/src/main/java/springmvc/controller/HomeController.java
@@ -0,0 +1,22 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/index")
+	public String home()
+	{
+		System.out.println("index page");
+		return "index";
+	}
+
+	@RequestMapping("/about")
+	public String about()
+	{
+		System.out.println("about page");
+		return"about";
+	}
+}
