61f9347d4c9d89587bdf70b72eb2fced2cfb83a3
Ankit Anand <ankitanandraj9563@gmail.com>
2025-09-03 15:06:25 +0530

    Update SpringDataMappingApplication.java

--- Mapping_In_SpringBoot/SpringDataMapping/src/main/java/com/mapping/example/SpringDataMappingApplication.java
+++ Mapping_In_SpringBoot/SpringDataMapping/src/main/java/com/mapping/example/SpringDataMappingApplication.java
@@ -18,6 +18,11 @@ public class SpringDataMappingApplication implements CommandLineRunner {
 	@Autowired
 	LaptopRepo laptopRepo;
 
+		@Autowired
+	TeacherRepo teacherRepo;
+	@Autowired
+	StbjectRepo stbjectRepo;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataMappingApplication.class, args);
 	}
@@ -43,5 +48,33 @@ public class SpringDataMappingApplication implements CommandLineRunner {
 
 		laptopRepo.save(laptop);
 		System.out.println("Student saved: " + saved.getStudentname());
+
+
+
+	// one to many            and               manyToOne
+
+		Teacher teacher=new Teacher();
+		teacher.setTeacherId(102);
+		teacher.setTeacherName("rohit kumar");
+		teacher.setTeacherAbout("this is good teacher");
+
+		Teacher save = teacherRepo.save(teacher);
+		System.out.println("teacher save..."+teacher.getTeacherName());
+
+		Stbject stbject=new Stbject();
+		stbject.setSuvjectId(6);
+		stbject.setSubjectName("physics Science");
+		stbject.setSubjectDuraction("8 Week");
+		stbject.setTeacher(teacher);
+
+		Stbject save1 = stbjectRepo.save(stbject);
+		System.out.println("Subject save...");
+
+
+
+
+
+
+		
 	}
 }
